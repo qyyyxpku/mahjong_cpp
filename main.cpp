4656d81448f108e78b8ea3bdf861a93b2a4eabb8
qyyyxpku <64321462+qyyyxpku@users.noreply.github.com>
2020-05-03 11:48:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,9 +20,37 @@ vector<string> hand;
 vector<string> shun_zi, ke_zi, gang_zi;
 
 int shu[28] = { 0 }, feng[5] = { 0 }, jian[4] = { 0 };//数牌按饼条万的1-9顺序排列，数组数字代表该牌的数量
-int shu_ting[28][6] = { 0 }, feng_ting[5] = { 0 }, jian_ting[4] = { 0};
-//数牌的6个数分别表示是否听该牌，是否能杠，是否能碰，是否左两位吃，是否右两位吃，是否左右吃
+int shu_remain[28] = { 0 }, feng_remain[5] = { 0 }, jian_remain[4] = { 0 };//数牌按饼条万的1-9顺序排列，数组数字代表该牌的数量
+int shu_quan[28][6] = { 0 }, feng_quan[5] = { 0 }, jian_quan[4] = { 0 };//数牌按饼条万的1-9顺序排列，数组数字代表该牌的鸣牌时的权重
+//数牌的6个数分别表示总权重，碰或杠的权重，连续吃在左权重，连续吃在右权重，分开吃在左权重，分开吃在右权重
+//eg.如手牌同时有2345万，4万连续吃在左权重即为45万吃的权重，为3万和6万剩余牌数量和，连续吃在右权重即34万吃，分开吃在右权重即24万吃
+int shu_quan_[28] = { 0 }, feng_quan_[5] = { 0 }, jian_quan_[4] = { 0 };//数牌按饼条万的1-9顺序排列，数组数字代表该牌本身的权重，仅用于散牌判断出牌顺序
+int shu_ting[28][6] = { 0 }, feng_ting[5][2] = { 0 }, jian_ting[4][2] = { 0 };
+//数牌的6个数分别表示是否听该牌，是否能碰，是否能杠，是否左两位吃，是否右两位吃，是否左右吃；风和箭的两个数分别为是否能碰，是否能杠
 
+void paiquanzhong() {
+    int sanpai = 10, weizhi = 1, type = 0;//权重值的数值，方便之后增加调整权重的内容，type是采用的鸣牌权重类型，此处是鸣牌总权重
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            shu_quan_[i * 9 + j] += shu_quan[i * 9 + j][type] * sanpai;
+            if (j == 1 || j == 9) {
+                shu_quan_[i * 9 + j] += 1;
+            }
+            else if (j == 2 || j == 8) {
+                shu_quan_[i * 9 + j] += 2;
+            }
+            else {
+                shu_quan_[i * 9 + j] += 3;
+            }
+        }
+    }
+    for (int i = 1; i <= 4; ++i) {
+        feng_quan_[i] += feng_quan[i];
+    }
+    for (int i = 1; i <= 3; ++i) {
+        jian_quan_[i] += jian_quan[i];
+    }
+}
 
 void canmingpai() {
     int l, r;
@@ -86,8 +114,110 @@ void canmingpai() {
     }
 }
 
+int shu_location(string s) { //返回数牌的位置
+    int num = s[1] - '0';
+    switch (s[0]) {
+    case 'B':
+        return num;
+    case 'T':
+        return num + 9;
+    case 'W':
+        return num + 18;
+    default:
+        return -1;
+    }
+}
+
+void del_remain(string stmp, int n) {//记牌器
+    int num = stmp[1] - '0';
+    switch (stmp[0]) {
+    case 'F':
+        feng_remain[num] -= n;
+        break;
+    case 'J':
+        jian_remain[num] -= n;
+        break;
+    default:
+        shu_remain[shu_location(stmp)] -= n;
+    }
+}
+
+void HU() {
+    if (hand.size == 2) {
+        if (hand.front == hand.back)return 1;
+        else return 0;
+    }
+    else
+        return 0;
+}
+/*if (HU) {
+    request.push_pack("HU")
+}*/
+
+void quanzhongzuixiao(string a) {
+    int shu_min = 1, feng_min = 1, jian_min = 1;//最小值下标
+    int shu_temp = shu_quan[1], feng_temp = feng_quan[1], jian_temp = jian_quan[1];//最小值
+    char kind, num;
+    for (int i = 2; i < 28; i++) {
+        if (shu_quan[i] < shu_temp) {//>=使得权重相同时，先打序号大的牌
+            shu_temp = shu_quan[i];
+            shu_min = i;
+        }
+    }
+    for (int i = 2; i < 4; i++) {
+        if (feng_quan[i] <= feng_temp) {
+            feng_temp = feng_quan[i];
+            feng_min = i;
+        }
+    }
+    for (int i = 2; i < 3; i++) {
+        if (jian_quan[i] <= jian_temp) {
+            jian_temp = jian_quan[i];
+            jian_min = i;
+        }
+    }
+    if (shu_temp <= feng_temp) {//权重相同，优先出数牌
+        if (shu_temp <= jian_temp) {
+            if ((shu_min - 1) / 9 == 0) {
+                kind = 'B';
+            }
+            else if ((shu_min - 1) / 9 == 1) {
+                kind = 'T';
+            }
+            else
+                kind = 'W';
+            num = (char)shu_min;
+        }
+        else {
+            kind = 'J';
+            num = (char)jian_min;
+        }
+    }
+    else if (feng_temp < jian_temp) {
+        kind = 'F';
+        num = (char)feng_min;
+    }
+    else {
+        kind = 'J';
+        num = (char)jian_min;
+    }
+    a = kind + num;
+    return a;
+}
+
 int main()
 {
+        //记牌器初始化
+    for (int i = 1; i < 28; i++) {
+        shu_remain[i] = 4;
+    }
+    for (int i = 1; i < 5; i++) {
+        feng_remain[i] = 4;
+    }
+    for (int i = 1; i < 4; i++) {
+        jian_remain[i] = 4;
+    }
+    
     int turnID;
     string stmp;
 
@@ -227,6 +357,7 @@ int main()
             }
         }
         canmingpai();
+        paiquanzhong();
         
         // 进行当前轮操作
         sin.clear();
